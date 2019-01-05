186f816a2080f457e34159cac3e0f51a96444227
am4am5 <36809996+am4am5@users.noreply.github.com>
2019-01-05 23:31:28 +0300

    Add files via upload

---
+++ off.sh
@@ -0,0 +1,2 @@
+ansible all -b -e "ansible_python_interpreter=/usr/bin/python3" -m shell -a "/usr/sbin/poweroff"
+
---
+++ reb.sh
@@ -0,0 +1,1 @@
+ansible all -b -e "ansible_python_interpreter=/usr/bin/python3" -m reboot
